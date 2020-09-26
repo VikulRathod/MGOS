f5dcf16f5365a03b8d8e998d59b0b8f98f40b5ec
jadhavbalaji123@gmail.com <jadhavbalaji123@gmail.com>
2020-09-26 12:36:34 +0530

    Demo folder added

---
+++ Server/ScoopenAPIModals/Demo/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoopenAPIModals.Demo
+{
+   public class Demo
+    {
+
+    }
+}
