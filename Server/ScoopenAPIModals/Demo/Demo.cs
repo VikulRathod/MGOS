2e38d777c66cfcee89baf7658c42333ddf828d66
jadhavbalaji123@gmail.com <jadhavbalaji123@gmail.com>
2020-09-26 12:51:09 +0530

    Added property monile number in demo class

--- Server/ScoopenAPIModals/Demo/Demo.cs
+++ Server/ScoopenAPIModals/Demo/Demo.cs
@@ -11,6 +11,7 @@ namespace ScoopenAPIModals.Demo
         public int Id { get; set; }
         public string  Name { get; set; }
         public string Gender { get; set; }
+        public int MobilenUMBER { get; set; }
 
     }
 }
